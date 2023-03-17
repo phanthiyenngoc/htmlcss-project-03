60bb15bb4f591e82ececf2963d3da23eebfa59be
techthaotran <tech.thaotran@gmail.com>
2023-03-17 14:07:01 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import LayoutDefault from "./layouts";
+import HomePageIndex from "./pages/home";
+import "../src/accessts/style/reset.css"
+import "../src/accessts/style/common.css"
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LayoutDefault>
+    <HomePageIndex/>
+      </LayoutDefault>
     </div>
   );
 }
